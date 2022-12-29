873cc2b32449409bbcc348ff706e1fc547a75965
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2022-12-29 08:29:14 +0500

    Initialize

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.code.contactapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.code.contactapp to javafx.fxml;
+    exports com.code.contactapp;
+}
